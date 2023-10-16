9268dba331a9f21f2edcaea664c320b654c3e420
betterthanhajin <betterthanhajin@icloud.com>
2023-10-16 20:44:51 +0900

    feat: chatting components 추가

---
+++ components/chatting-container.tsx
@@ -0,0 +1,112 @@
+import Image from 'next/image'
+export default function ChattingContainer() {
+  return (
+    <div>
+      <div className="flex justify-between px-4 py-5">
+        <h1 className="font-bold">채팅</h1>
+        <Image src="/icon/Vector.png" alt="profile" width={24} height={24} />
+        <Image
+          src="/icon/Notification.svg"
+          alt="profile"
+          width={24}
+          height={24}
+        />
+      </div>
+      <div className="px-4 py-5 flex justify-between">
+        <div>
+          <Image src="/icon/profile.svg" alt="profile" width={40} height={40} />
+        </div>
+        <div>
+          <p>
+            <strong>요우</strong>군자동 * 1주전
+          </p>
+          <p>감사합니다!!조심히가세요!!</p>
+        </div>
+        <div>
+          <Image
+            src="/icon/chatItem.svg"
+            alt="chatItem"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+      <div className="px-4 py-5 flex justify-between">
+        <div>
+          <Image src="/icon/profile.svg" alt="profile" width={40} height={40} />
+        </div>
+        <div>
+          <p>
+            <strong>요우</strong>군자동 * 1주전
+          </p>
+          <p>감사합니다!!조심히가세요!!</p>
+        </div>
+        <div>
+          <Image
+            src="/icon/chatItem.svg"
+            alt="chatItem"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+      <div className="px-4 py-5 flex justify-between">
+        <div>
+          <Image src="/icon/profile.svg" alt="profile" width={40} height={40} />
+        </div>
+        <div>
+          <p>
+            <strong>요우</strong>군자동 * 1주전
+          </p>
+          <p>감사합니다!!조심히가세요!!</p>
+        </div>
+        <div>
+          <Image
+            src="/icon/chatItem.svg"
+            alt="chatItem"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+      <div className="px-4 py-5 flex justify-between">
+        <div>
+          <Image src="/icon/profile.svg" alt="profile" width={40} height={40} />
+        </div>
+        <div>
+          <p>
+            <strong>요우</strong>군자동 * 1주전
+          </p>
+          <p>감사합니다!!조심히가세요!!</p>
+        </div>
+        <div>
+          <Image
+            src="/icon/chatItem.svg"
+            alt="chatItem"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+      <div className="px-4 py-5 flex justify-between">
+        <div>
+          <Image src="/icon/profile.svg" alt="profile" width={40} height={40} />
+        </div>
+        <div>
+          <p>
+            <strong>요우</strong>군자동 * 1주전
+          </p>
+          <p>감사합니다!!조심히가세요!!</p>
+        </div>
+        <div>
+          <Image
+            src="/icon/chatItem.svg"
+            alt="chatItem"
+            width={40}
+            height={40}
+          />
+        </div>
+      </div>
+    </div>
+  )
+}
