912c3f7711d66918527d45d3c0ca9d7afb4bf19d
betterthanhajin <betterthanhajin@icloud.com>
2023-10-19 20:13:12 +0900

    feat: 동네생활 페이지 추가

---
+++ app/neighlife/page.tsx
@@ -0,0 +1,13 @@
+import ListFooter from '@/components/list-footer'
+import ListHeader from '@/components/list-header'
+import NeighContainer from '@/components/neigh-container'
+
+export default function NeighLife() {
+  return (
+    <main className="w-full h-full flex flex-col">
+      <ListHeader />
+      <NeighContainer />
+      <ListFooter />
+    </main>
+  )
+}
