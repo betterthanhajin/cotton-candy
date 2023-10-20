373182c3a756608a409b67ea4f71801a8ad286a4
betterthanhajin <betterthanhajin@icloud.com>
2023-10-20 11:12:30 +0900

    feat: list footer 위치조정

--- components/list-footer.tsx
+++ components/list-footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function ListFooter() {
   return (
-    <div className="py-2 bg-white w-full flex justify-around border-t border-t-[#d9d9d9] select-none fixed bottom-0">
+    <div className="py-2 bg-white w-full flex justify-around border-t border-t-[#d9d9d9] select-none">
       <div className="text-center flex justify-center flex-col items-center">
         <Image src="/icon/Home.svg" alt="Home" width={24} height={24} />
         <span className="text-[10px]">홈</span>
