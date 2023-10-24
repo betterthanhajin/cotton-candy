2e52fab08a14745547b6fecd7dcaee565831edcb
betterthanhajin <betterthanhajin@icloud.com>
2023-10-24 15:23:07 +0900

    feat: 상세페이지 추가

--- app/detail/page.tsx
+++ app/detail/page.tsx
@@ -6,7 +6,11 @@ export default function DetailPage() {
   return (
     <main className="w-full h-full flex flex-col">
       <DetailSlider />
-      <DetailContainer />
+      <DetailContainer
+        username="감성탐방러"
+        localname="중랑구 면목동"
+        boardtitle="소니 Wh-1000xm5 실버 팝니다."
+      />
       <DetailFooter />
     </main>
   )
