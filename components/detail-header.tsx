003f94962c0bb40fdd08b8dfab8b175ce578a4fc
betterthanhajin <betterthanhajin@icloud.com>
2023-10-28 16:46:42 +0900

    feat: 제품 상 페이지 userRounter 적용

--- components/detail-header.tsx
+++ components/detail-header.tsx
@@ -1,23 +1,42 @@
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 export default function DetailHeader() {
+  const router = useRouter()
   return (
     <div className="flex absolute top-0 p-4 w-full justify-between">
-      <div className="flex">
-        <div>
-          <Image src="/icon/aaa.svg" alt="Home" width={24} height={24} />
-        </div>
-        <div>
-          <Image src="/icon/bbb.svg" alt="Home" width={24} height={24} />
-        </div>
+      <div className="flex text-white">
+        <button onClick={() => router.back()}>
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line
+              x1="17.5303"
+              y1="2.53033"
+              x2="7.53033"
+              y2="12.5303"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            />
+            <line
+              x1="7.53033"
+              y1="11.4697"
+              x2="17.5303"
+              y2="21.4697"
+              stroke="currentColor"
+              strokeWidth="1.5"
+            />
+          </svg>
+        </button>
       </div>
       <div className="flex">
-        <div>
-          <Image src="/icon/ccc.svg" alt="Home" width={24} height={24} />
-        </div>
-        <div>
+        <button>
           <Image src="/icon/ddd.svg" alt="Home" width={24} height={24} />
-        </div>
+        </button>
       </div>
     </div>
   )
