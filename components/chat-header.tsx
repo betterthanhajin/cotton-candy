1ac7d9a9e451616c280dbbeffd5f13a50a8144e8
betterthanhajin <betterthanhajin@icloud.com>
2023-10-28 16:34:44 +0900

    feat: 목업처리

--- components/chat-header.tsx
+++ components/chat-header.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 export default function ChatHeader() {
+  const router = useRouter()
   return (
-    <div className="flex p-2 items-center justify-between">
-      <div className="rotate-180">
+    <div className="flex p-2 items-center justify-between select-none">
+      <button className="rotate-180" onClick={() => router.back()}>
         <Image src="/icon/moreView.svg" alt="arrow" width={20} height={20} />
-      </div>
-      <div>User 1</div>
+      </button>
+      <div>ANBER</div>
       <div></div>
     </div>
   )
