e85b261207e356df91ab576c623ab6153020ccd4
betterthanhajin <betterthanhajin@icloud.com>
2023-10-28 16:31:37 +0900

    feat: 목업내용작성

--- components/chat-detail.tsx
+++ components/chat-detail.tsx
@@ -1,24 +1,30 @@
 import Image from 'next/image'
 export default function ChatDetailContainer() {
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full select-none">
       <div className="flex p-2 border-b border-t gap-2">
         <div>
-          <Image src="/icon/Frame 1.svg" alt="Home" width={55} height={55} />
+          <img
+            src="/mock/item5.jpeg"
+            alt="sell item"
+            width={55}
+            height={55}
+            className="rounded-lg"
+          />
         </div>
         <div className="p-1">
-          <span className="font-bold">빌려주는 중 !</span> 산악자전거 장기대여
-          가능합니다~!
+          <span className="font-bold">할인 중!</span> ANBER 귀엽고
+          멍하고있는공룡인형 안고자는 쿠션
           <div>
-            <strong>5,000원</strong>
+            <strong>29,580원</strong>
           </div>
         </div>
       </div>
       <div className="p-2 flex gap-4 items-end justify-end w-full">
-        <div className="text-sm text-gray-500 w-1/3">오후 3:59</div>
+        <div className="text-sm text-gray-500 w-1/3 text-right">오후 3:59</div>
         <div className="bg-orange-400 rounded-md text-white text-sm px-3 py-2 flex w-2/3">
-          동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화
-          삼천리 화려강산 대한사람 대한으로 길이 보전하세
+          안녕하세요 인형 구매하려는데 인형 오늘 입금하면 언제쯤 받아볼 수
+          있을지 알 수 있을까요?
         </div>
       </div>
       <div className="p-2 flex gap-4 items-end justify-start w-full">
@@ -29,8 +35,8 @@ export default function ChatDetailContainer() {
           height={44}
         />
         <div className="bg-gray-200 rounded-md text-black text-sm px-3 py-2 flex min-w-min  w-2/3">
-          동해물과백두산이 마르고 닳도록 하느님이 보우하사 우리나라만세 무궁화
-          삼천리 화려강산 대한사람 대한으로 길이 보전하세
+          안녕하세요! 쿠팡 로켓배송이나 오늘의 픽업으로 배송되기 때문에 당일 날
+          주문 하시고 나서 다음 날 받으실 수 있을 것 같아요!
         </div>
         <div className="text-sm text-gray-500  w-1/3">오후 8:46</div>
       </div>
