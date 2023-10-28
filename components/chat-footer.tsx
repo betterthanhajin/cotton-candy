942d8fa9795ed54e5edcbaac585364faf3198cc9
betterthanhajin <betterthanhajin@icloud.com>
2023-10-28 16:33:49 +0900

    feat: 푸터 목업 이미지 작성

--- components/chat-footer.tsx
+++ components/chat-footer.tsx
@@ -1,18 +1,28 @@
+import { todoAlert } from '@/lib/todo'
 import Image from 'next/image'
 export default function ChatFooter() {
   return (
-    <div className="flex p-2 items-center justify-between w-full gap-2 border-t">
-      <button>
+    <div className="flex p-2 items-center justify-between w-full gap-2 border-t select-none">
+      <button
+        onClick={() => {
+          todoAlert()
+        }}
+      >
         <Image src="/icon/camera.svg" alt="Home" width={37} height={24} />
       </button>
       <div className="w-full">
         <input
           type="text"
-          className="w-full rounded-md bg-gray-200 h-10 border-none"
+          className="w-full rounded-md bg-gray-200 h-10 border-none p-2"
         />
       </div>
-      <button>
-        <Image src="/icon/Ellipse 1.svg" alt="Home" width={32} height={32} />
+      <button
+        onClick={() => {
+          todoAlert()
+        }}
+        className="w-10 h-10 bg-[#FF8B03] shrink-0 flex items-center justify-center rounded-full"
+      >
+        <Image src="/icon/send-arrow.svg" alt="Home" width={32} height={32} />
       </button>
     </div>
   )
