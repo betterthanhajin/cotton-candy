54eb0c06fd89307381a923752cfcd0e8c275917a
betterthanhajin <betterthanhajin@icloud.com>
2023-10-28 16:44:57 +0900

    feat: 제품 상세 페이지 푸터 목업 처리

--- components/detail-footer.tsx
+++ components/detail-footer.tsx
@@ -1,19 +1,25 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function DetailFooter() {
   return (
-    <div className="p-5 flex justify-between items-center">
-      <div>
+    <div className="p-5 flex justify-between items-center sticky bottom-0 bg-white">
+      <button>
         <Image src="/icon/heart.svg" alt="heart" width={24} height={24} />
-      </div>
-      <p className="font-bold">
-        360,000원
+      </button>
+      <p className="font-bold text-center">
+        29,580원
         <br />
         <span className="text-gray-400">가격 제안 불가</span>
       </p>
-      <div className="p-2 bg-orange-500 text-white w-20 h-8 rounded-md flex justify-center items-center">
-        채팅하기
-      </div>
+      <Link
+        href="https://www.coupang.com/vp/products/7308290773?itemId=18717224596&vendorItemId=86016835685"
+        target="_blank"
+      >
+        <button className="p-2 bg-orange-500 text-white w-20 h-8 rounded-md flex justify-center items-center">
+          구매하기
+        </button>
+      </Link>
     </div>
   )
 }
